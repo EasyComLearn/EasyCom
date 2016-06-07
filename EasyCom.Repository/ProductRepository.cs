7f5504367ae9eb50be5dbe30d0667903a1a87fc2
email2vignesh <email2vignesh@gmail.com>
2016-06-07 18:48:34 +0530

    Initial Code

---
+++ EasyCom.Repository/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyCom.Api.Repository.Interfaces;
+using EasyCom.Api.Types;
+
+namespace EasyCom.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
